eec7a73cfcd794f9c5c5fe22cbae347802d0d820
VHSgunzo <wosganzo@gmail.com>
2023-11-16 21:10:24 +0300

    v0.0.3

--- PKGBUILD
+++ PKGBUILD
@@ -1,6 +1,6 @@
 # Maintainer: VHSgunzo <vhsgunzo.github.io>
 pkgname='moninfo'
-pkgver='0.0.2'
+pkgver='0.0.3'
 pkgrel='1'
 pkgdesc="Show info about primary monitor output in Rust"
 arch=("x86_64")
--- src/main.rs
+++ src/main.rs
@@ -58,16 +58,14 @@ fn main() {
         },
         2 => {
             match exec_args[1].as_str() {
-                "-m"|"--mhfsize" => {
-                    println!("{}", primary_monitor.size().height / mhfratio)
-                },
+                "-m"|"--mhfsize" => { println!("{}", primary_monitor.size().height / mhfratio) },
                 "-x"|"--xrandr" => {
                     let mut config = "".to_string();
                     for monitor in event_loop.available_monitors() {
                         let monitor_name = monitor.name().unwrap();
+                        let monitor_size = monitor.size();
                         let monitor_position = monitor.position();
                         let video_mode = monitor.video_modes().next().unwrap();
-                        let video_mode_size = video_mode.size();
                         config.push_str("--output ");
                         if monitor_name == primary_monitor_name {
                             config.push_str(format!("{monitor_name} --primary ").as_str())
@@ -76,25 +74,17 @@ fn main() {
                         }
                         config.push_str(
                             format!("--mode {}x{} --pos {}x{} --rate {} --rotate normal ",
-                            video_mode_size.width, video_mode_size.height, 
+                            monitor_size.width, monitor_size.height, 
                             monitor_position.x, monitor_position.y, 
                             video_mode.refresh_rate()).as_str()
                         );
                     }
                     println!("{config}");
                 },
-                "-h"|"--help" => {
-                    print_help()
-                },
-                _ => {
-                    eprintln!("Error: invalid argument!");
-                    print_help()
-                },
+                "-h"|"--help" => { print_help() },
+                _ => { print_help() },
             }
         },
-        _ => {
-            eprintln!("Error: invalid argument!");
-            print_help()
-        }
+        _ => { print_help() }
     }
 }
